2051dd74d2eb7b6defb1317d0684cd3d4f9af03a
LuciusGabriel <lucius_gabriel@hotmail.com>
2023-04-23 21:33:14 -0300

    Melhorias no CSS e Progresso no JS

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,11 @@
-const pokemonImg = document.querySelector('.pokemon_img')
-const searchPokemon = document.querySelector('.search_pokemon')
+const pokemonNAME = document.querySelector('.pokemon_name')
+const pokemonTYPE1 = document.querySelector('.pokemon_type_1')
+const pokemonTYPE2 = document.querySelector('.pokemon_type_2')
+const pokemonID = document.querySelector('.pokemon_id')
+const pokemonWEIGHT = document.querySelector('.pokemon_weight')
+const pokemonIMG = document.querySelector('.pokemon_img')
 
+const card = document.querySelector('.card')
 
 const buscaPokemon = async (pokemon) =>{
     const apiResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
@@ -8,7 +13,41 @@ const buscaPokemon = async (pokemon) =>{
     return data;
 }
 
-searchPokemon.addEventListener('submit', (event)=>{
-    event.preventDefault();
+const renderPokemon = async (pokemon) =>{
 
-})
+    const data = await buscaPokemon(pokemon)
+
+    pokemonNAME.textContent = data.name; // Determina o NAME do pokemon
+    pokemonTYPE1.textContent = data.types[0].type.name; // Determina o TYPE do pokemon
+    if(data.types.length == 2){
+        pokemonTYPE2.textContent = data.types[1].type.name;
+    }else{
+        pokemonTYPE2.style.display = 'none'
+    } //Verificação para pokémon que possuem apenas um type 
+
+    const typePokemon = data.types[0].type.name;
+    const typesPokemon = ['normal', 'fire', 'water', 'grass', 'flying', 'fighting', 'poison', 'electric', 'ground', 'rock', 'psychic', 'ice', 'bug', 'ghost', 'steel', 'dragon', 'dark', 'fairy']
+    
+    switch(typePokemon){
+        case 'water':
+            card.classList.remove(typesPokemon)
+            card.classList.add('water')
+            break
+        case 'fire':
+            card.classList.remove('water', 'grass')
+            card.classList.add('fire')
+            break
+        case 'grass':
+            card.classList.remove('water', 'fire')
+            card.classList.add('grass')
+    }
+
+    
+    pokemonIMG.src = data['sprites']['versions']['generation-v']['black-white']['front_default'];
+    pokemonID.textContent = data.id; // Determina a ID do pokemon
+    pokemonWEIGHT.textContent = data.weight
+
+
+}
+
+renderPokemon(6)
